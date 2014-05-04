99b69b7a6b39e2d29d9e48db29ef832932d604d5
Fikri Rasyid <fikrirasyid@gmail.com>
2014-05-05 06:23:22 +0700

    adding template tag, abstracting the shortcode

--- testimonials-submission-template.php
+++ testimonials-submission-template.php
@@ -1,10 +1,38 @@
 <?php
 /**
- *
+ * Get testimonials submission form
+ * 
+ * @author Fikri Rasyid
+ * 
+ * @return string
  */
-function testimonials_submission_shortcode( $atts = array(), $content = null ){
+function get_testimonials_submission(){
 	$testimonials_submission = new Testimonials_Submission_Public();
 	$form = $testimonials_submission->get_form();
+
+	return $form;	
+}
+
+/**
+ * Print testimonials submission form
+ * 
+ * @author Fikri Rasyid
+ * 
+ * @return void
+ */
+function testimonials_submission(){
+	echo get_testimonials_submission();
+}
+
+/**
+ * Adding shortcode for displaying testimonials submission form on the_content
+ * 
+ * @author Fikri Rasyid
+ * 
+ * @return string
+ */
+function testimonials_submission_shortcode(){
+	$form = get_testimonials_submission();
 
 	return $form;
 }
